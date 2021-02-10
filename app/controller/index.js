78cf5efa6dde471735a6caa0028b2a321427aa44
Admin <admin@Admins-iMac.local>
2021-02-09 19:00:08 -0500

    initial commit

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,5 +1,8 @@
+const urlService = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=5AAMG971G6GEX04HYE8Jhqw0qu5ITQho';
+const urlImages = 'https://static01.nyt.com/images/';
+let arr_data = [];
 function prueba(){
-    fetch('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=API_KEY')
+    fetch(urlService)
     .then(response => {
             if (response.status !== 200) {
                 console.log('Looks like there was a problem. Status Code: ' +
@@ -8,8 +11,9 @@ function prueba(){
             }
 
             // Examine the text in the response
-            response.json().then(function(data) {
+            response.json().then(data => {
                 console.log(data);
+                arr_data = data
             });
         }
     )
@@ -18,3 +22,6 @@ function prueba(){
     });
 }
 prueba()
+
+
+
